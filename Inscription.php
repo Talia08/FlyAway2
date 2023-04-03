d71fd61ddac9bfd404f86e5add3270ae5a62c8db
Talia08 <talialivio0@gmail.com>
2023-04-03 16:08:28 +0200

    update

--- Inscription.php
+++ Inscription.php
@@ -1,93 +1,36 @@
 <?php
 
-$user = 'root';
-$pass = '';
-$database = 'formulaire_inscription';
+// Récupération des données du formulaire
+$nom = $_POST['nom'];
+$prenom = $_POST['prenom';]
+$email = $_POST['email'];
+$mot_de_passe = $_POST['mot_de_passe'];
+$date_naissance = $_POST['date_naissance'];
+$telephone = $_POST['telephone'];
+$sexe = $_POST['sexe'];
 
-$query = mysqli_connect($_SERVER, $user, $pass, $database)
+// Connexion à la base de données
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "flyaway";
 
-if()
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+// Insertion des données dans la base de données
+$sql = "INSERT INTO formulaire_inscription (nom, prenom, date_naissance, telephone, email, sexe, mot_de_passe)
+        VALUES ('$nom', '$prenom', '$date_naissance', '$telephone', '$email', '$sexe', '$mot_de_passe')";
+
+if (mysqli_query($conn, $sql)) {
+  echo "Inscription réussie!";
+} else {
+  echo "Erreur d'inscription: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
 
 ?>
 
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Chivo+Mono:wght@200&family=Lato:wght@300&family=Raleway:ital@1&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="reset.css">
-    <link rel="stylesheet" href="lib.css">
-    <link rel="stylesheet" href="Init.php">
-    <link rel="stylesheet" href="Inscription.css">
-    <title>Inscription</title>
-</head>
-<body>
-
-    <header>
-        <a href="Home_page.html">
-        <img class="logo" src="./Image/FlyAway_logo.png" alt="logo">
-        </a>
-        <div class="search">
-        <img class="icone-search" src="./icône/magnifying-glass-solid 2.svg" alt="">
-        <form action="">
-            <input type="text" placeholder="Recherche...">
-        </form>
-        </div>
-        <ul>
-            <li><a href=""><button class="btn-rouge">Publier</button></a></li>
-            <li><a href=""><button class="btn-rouge">Mon compte</button></a></li>
-            <li><a href=""><button class="btn-rouge"><a href="Connexion.php">Connexion</a></button></a></li>
-        </ul>
-    </header>
-
-    <main>
-        <h1>Inscrit-toi à FlyAway</h1>
-
-        <?php echo $content; ?>
-
-        <form class="inscription" method="post">
-            <input type="text" placeholder="Nom et prénom" required>
-            <br>
-            <input type="date" placeholder="Date de naissance" required>
-            <br> 
-            <input type="number" placeholder="Téléphone" required>
-            <br>
-            <input type="text" placeholder="Adresse Mail" required>
-            <br>
-            <input type="number" placeholder="Mot de passe" required>
-            <br>
-            <input type="submit" value="Inscription">
-        </form>
-    </main>
-
-    <footer>
-        <a href="Home_page.html">
-        <img class="logo" src="./Image/FlyAway_logo.png" alt="">
-        </a>
-        <ul>
-            <li><a class="propos" href="">A propos</a></li>
-            <li><a href="">Mentions légales</a></li>
-            <li><a href="">Conditions d'utilisations</a></li>
-            <li><a href="">CGU</a></li>
-            <li><a href="">Politique de confidentialité</a></li>
-        </ul>
-        <ul>
-            <li><a class="footer-compte" href="">Mon compte</a></li>
-            <li><a href="">Mes abonnements</a></li>
-            <li><a href="">Mes favoris</a></li>
-            <li><a href="">Mes enregistrements</a></li>
-            <li><a href="">Mes postes</a></li>
-        </ul>
-        <div class="newsletter">
-            <p>Je m'inscris à la newsletter</p>
-            <input type="text" placeholder="Email">
-            <input type="submit" value="Inscription">
-            <br>
-            <input type="checkbox" id="checkbox" name="checkbox" value=">Valeur1">
-            <label for="checkbox">Je souhaite recevoir les informations et les nouveautés de FlyAway</label>
-    </footer>
-    
-</body>
-</html>
